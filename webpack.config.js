358d95855312021758d86f9f671a94eacd2e1074
William Francis <william@gradconnection.com>
2017-04-24 16:38:18 +0200

    Upgraded web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -9,17 +10,34 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.scss$/, use: ['style-loader','css-loader', 'sass-loader'] }
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    loader: ['css-loader', 'sass-loader'],
+                    publicPath: "/dist"
+                })
+            }
         ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname,"dist"),
+        compress: true,
+        port:8088,
+       // stats: "errors-only",
+        open:true
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'project from config',
-            minify: {
-                collapseWhitespace: true
-            },
+            title: 'project from config 2',
+
             hash: true,
             template: './src/index.ejs'
-        }
-        )]
+        }),
+        new ExtractTextPlugin({
+            filename: "app.css",
+            disable: false,
+            allChunks: true
+        })
+    ]
 }
